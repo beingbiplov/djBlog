ff6c034a54c3a69bf1034cf6c56bc5df1fdfacf0
beingbiplov <beingbiplov@gmail.com>
2020-09-15 17:28:25 +0545

    design index page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-	return render(request, 'core/index.html')
+
+	# Dummy data for front-end
+	posts = [
+		{
+			'title' : 'Blog post title.',
+			'description' : 'This is a short description of the blog post consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+			'content' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+			'createdAt' : datetime.now().strftime('%D %H:%M'),
+		},
+		{
+			'title' : 'New post title.',
+			'description' : 'This is a short description of the blog post',
+			'content' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+			'createdAt' : datetime.now().strftime('%D %H:%M'),
+		}
+	]
+
+	context = {
+		'posts' : posts
+	}
+
+	return render(request, 'core/index.html', context)
