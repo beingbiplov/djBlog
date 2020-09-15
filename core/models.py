e3cea07063f2808546a29328c21ddb0f4c470633
beingbiplov <beingbiplov@gmail.com>
2020-09-15 18:23:02 +0545

    creade model for blog posts

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=150, blank=False, null=False)
+	description = models.TextField(blank=False, null=False)
+	content = models.TextField(blank=False, null=False)
+	createdAt = models.DateField(auto_now=True)
+	thumbnail = models.ImageField(default="placeholder_thumbnail.png", upload_to='thumbnail')
+
+
+	def __str__(self):
+		return self.title
