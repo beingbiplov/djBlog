e118b4b62826b25f1dbdffa7cf412cf61a437a50
beingbiplov <beingbiplov@gmail.com>
2020-09-15 19:49:41 +0545

    handle details view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexListView
+from .views import indexListView, blogDetailView
 
 urlpatterns = [
     path('', indexListView.as_view(), name='index'),
+    path('details/<int:pk>', blogDetailView.as_view(), name='details')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class indexListView(ListView):
 	model = Blog
 	template_name = 'core/index.html'
 	context_object_name= 'posts'
 	ordering = ['-createdAt']
+
+class blogDetailView(DetailView):
+	model = Blog
+	template_name = 'core/blog_details.html'
+	context_object_name= 'post'
