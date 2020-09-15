23c8df38465cb22c16ad091a7aacae582a129642
beingbiplov <beingbiplov@gmail.com>
2020-09-15 20:13:50 +0545

    handle delete view

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import indexListView, blogDetailView
+from .views import indexListView, blogDetailView, blogDeleteView
 
 urlpatterns = [
     path('', indexListView.as_view(), name='index'),
-    path('details/<int:pk>', blogDetailView.as_view(), name='details')
+    path('details/<int:pk>', blogDetailView.as_view(), name='details'),
+    path('delete/<int:pk>', blogDeleteView.as_view(), name='delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
 
 class indexListView(ListView):
 	model = Blog
@@ -12,3 +12,9 @@ class blogDetailView(DetailView):
 	model = Blog
 	template_name = 'core/blog_details.html'
 	context_object_name= 'post'
+
+class blogDeleteView(DeleteView):
+	model = Blog
+	template_name = 'core/delete.html'
+	context_object_name= 'post'
+	success_url = '/'
