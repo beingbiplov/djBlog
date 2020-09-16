71e9500d418cf437d7f098e0f99876562e14cb16
beingbiplov <beingbiplov@gmail.com>
2020-09-16 12:22:31 +0545

    add absolute url to blog model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Blog(models.Model):
 	title = models.CharField(max_length=150, blank=False, null=False)
@@ -10,3 +11,6 @@ class Blog(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('details', kwargs={'pk': self.pk})
